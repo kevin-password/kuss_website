7f25a743fa83c1465739015ff20df5a2fa48f48f
kevin-password <2024ambchb085f@kab.ac.ug>
2026-06-24 13:32:04 +0300

    Update settings.py

--- kuss_website/settings.py
+++ kuss_website/settings.py
@@ -46,11 +46,13 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processors.is_treasurer_context',
+                 'core.context_processors.leadership_context',
+],
             ],
         },
     },
